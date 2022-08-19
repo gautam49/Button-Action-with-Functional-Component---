872abe8b7e1a45349d64be9dbffd714cf5d15481
gautam <gautampal560@gmail.com>
2022-08-19 21:49:59 +0530

    19 august 2022

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,36 @@
-import React, {Component, useState} from "react";
-import "./../styles/App.css";
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
-function App() {
-  return (
-    <div id="main">
-      // Do not alter the main div
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hide: false
+    };
+  }
+  handleClick() {
+    this.setState({
+      hide: !this.state.hide
+    });
+  }
+
+  render() {
+    return (
+      <div id="main">
+        <button id="click" onClick={() => this.handleClick()}>
+          button
+        </button>
+        {this.state.hide ? (
+          <p id="para">
+            Hello, I've learnt to use the full-stack evaluation tool. This makes
+            me so happy
+          </p>
+        ) : (
+          ""
+        )}
+      </div>
+    );
+  }
 }
-
 
 export default App;
